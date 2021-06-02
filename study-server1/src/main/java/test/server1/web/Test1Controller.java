2b6274a0bda9964ccf2958d9d72392f5d6749161
sunyang <956086341@qq.com>
2021-06-02 19:33:29 +0800

    0602 19:33

--- study-server1/src/main/java/test/server1/web/Test1Controller.java
+++ study-server1/src/main/java/test/server1/web/Test1Controller.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.lang.reflect.Field;
+
 /**
  * @author sunYang
  * @Description:
@@ -19,6 +21,24 @@ public class Test1Controller {
     @GetMapping("/t1")
     public Mono<String> t1(){
         return Mono.just("1111111111111");
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Class cache = Integer.class.getDeclaredClasses()[0];
+        Field c = cache.getDeclaredField("cache");
+        c.setAccessible(true);
+        Integer[] array = (Integer[]) c.get(cache);
+        // array[129] is 1
+        array[130] = array[129];
+        // Set 2 to be 1
+        array[131] = array[129];
+        // Set 3 to be 1
+
+        Integer a = 1;
+
+        if(a.equals(1) && a.equals(2) && a.equals(3)){
+            System.out.println("Success");
+        }
     }
 
 }
