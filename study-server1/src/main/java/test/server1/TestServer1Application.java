ed986ad92200445b8a99bc37ef939c4e3f3a7b61
sunyang <956086341@qq.com>
2021-06-02 18:55:44 +0800

    init

---
+++ study-server1/src/main/java/test/server1/TestServer1Application.java
@@ -0,0 +1,20 @@
+package test.server1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sunYang
+ * @Description:
+ * @Title: TestServer1Application
+ * @Package test.server1
+ * @date 2021-06-0218:18
+ */
+@SpringBootApplication
+public class TestServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestServer1Application.class,args);
+    }
+
+}
---
+++ study-server1/src/main/java/test/server1/web/Test1Controller.java
@@ -0,0 +1,24 @@
+package test.server1.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author sunYang
+ * @Description:
+ * @Title: Test1Controller
+ * @Package test.server1.web
+ * @date 2021-06-0218:47
+ */
+@RestController
+@RequestMapping("/test1")
+public class Test1Controller {
+
+    @GetMapping("/t1")
+    public Mono<String> t1(){
+        return Mono.just("111111111111");
+    }
+
+}
